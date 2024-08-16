36082ff0ef26cd0e2c345060542189a5138aa13e
朱继超 <984065974@qq.com>
2024-08-16 09:52:03 +0800

    update config&readme

--- iOS/EasemobScenariosDemo/EasemobScenariosDemo/CustomConstants/PublicDefines.swift
+++ iOS/EasemobScenariosDemo/EasemobScenariosDemo/CustomConstants/PublicDefines.swift
@@ -7,13 +7,12 @@
 
 import Foundation
 
-// easemob#easeim
-public let AppKey: String = "easemob#1v1videochat"
+// 您的Application Key
+public let AppKey: String = <#您的App Key#>
+
 //最好使用自己的Appserver地址，尽量不要使用此地址，此地址只用于快速跑通Demo,此Demo对应Appserver是开源的具体参看demo readme
-public let ServerHost: String = "https://a1-appserver.easemob.com"
-//"https://a1-appserver.easemob.com"
+public let ServerHost: String = <#您的Appserver地址#>
 
 // https://docs.agora.io/en/agora-chat/get-started/enable?platform=ios
 // Create a new app in the Agora Console, and use the App Id in the following code
-public let AgoraAppId: String = "15cb0d28b87b425ea613fc46f7c9f974"
-//"15cb0d28b87b425ea613fc46f7c9f974"
+public let AgoraAppId: String = <#您的Agora App Id#>
